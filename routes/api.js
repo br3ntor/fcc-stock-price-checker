778f98cd38f0bba224bc4253892345a1a8aee865
Glitch (fcc-stocks) <none>
2020-08-29 05:12:00 +0000

    Used the glitch file format button, that's all

--- routes/api.js
+++ routes/api.js
@@ -6,47 +6,48 @@
  *
  */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-const stockAPI = require('./stockAPI');
-const stockHandler = require('../controllers/stockHandler');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
+const stockAPI = require("./stockAPI");
+const stockHandler = require("../controllers/stockHandler");
 
 const CONNECTION_STRING = process.env.DB;
 
 module.exports = function(app) {
   MongoClient.connect(CONNECTION_STRING, function(err, db) {
     if (err) throw err;
-    console.log('Connected successfully to db server');
+    console.log("Connected successfully to db server");
 
-    const dbController = new stockHandler(db, 'myStocks');
+    const dbController = new stockHandler(db, "myStocks");
 
-    app.route('/api/stock-prices').get(function(req, res) {
+    app.route("/api/stock-prices").get(function(req, res) {
       const stock = req.query.stock;
 
       // The x-forwarded-for header should get replaced by req.headers.host for test requests
-      const ip = req.headers['x-forwarded-for']
-        ? req.headers['x-forwarded-for'].split(',')[0]
-        : req.headers.host.split(':')[0];
+      const ip = req.headers["x-forwarded-for"]
+        ? req.headers["x-forwarded-for"].split(",")[0]
+        : req.headers.host.split(":")[0];
 
       const like = req.query.like;
       const voteIp = like ? ip : false;
-      
+
       // Handle edge case of two same stocks being compared
       if (Array.isArray(stock)) {
         if (stock[0] === stock[1]) {
-          return res.send('Please enter two DIFFERNT stocks. Thanks you.');
+          return res.send("Please enter two DIFFERNT stocks. Thanks you.");
         }
       }
 
       // Get quote for stock
       stockAPI(stock)
-        .then(data => {        
-          // Handle one stock          
+        .then(data => {
+          // Handle one stock
           if (!Array.isArray(data)) {
             dbController.findOne(data.symbol).then(doc => {
-              const validVoteIp = doc && !doc.stockData.voters.includes(ip) && like ? ip : false;
+              const validVoteIp =
+                doc && !doc.stockData.voters.includes(ip) && like ? ip : false;
 
               if (doc) {
                 dbController.updateOne(data, validVoteIp).then(r => {
@@ -55,7 +56,9 @@ module.exports = function(app) {
                       stockData: {
                         stock: data.symbol,
                         price: data.latestPrice,
-                        likes: validVoteIp ? doc.stockData.likes + 1 : doc.stockData.likes
+                        likes: validVoteIp
+                          ? doc.stockData.likes + 1
+                          : doc.stockData.likes
                       }
                     });
                   }
@@ -76,10 +79,10 @@ module.exports = function(app) {
             });
 
             // Handle two stocks
-          } else {            
-            dbController.findTwo(data).then(docs => {              
+          } else {
+            dbController.findTwo(data).then(docs => {
               if (docs.length === 0) {
-                console.log('Neither stock in database');
+                console.log("Neither stock in database");
                 dbController.insertMany(data, voteIp).then(r => {
                   const response = doubleResponseObject(r.ops);
                   res.json(response);
@@ -87,12 +90,19 @@ module.exports = function(app) {
               }
 
               if (docs.length === 1) {
-                console.log('One of the stocks is in the database');
-                const stockInDB = docs[0].stockData.stock === data[0].symbol ? data[0] : data[1];
-                const stockNotInDB = docs[0].stockData.stock === data[0].symbol ? data[1] : data[0];
-                const validVoteIp = !docs[0].stockData.voters.includes(ip) && like ? ip : false;
-                
-                // Need to check if stocks are the same and either compare them or say they are the same                
+                console.log("One of the stocks is in the database");
+                const stockInDB =
+                  docs[0].stockData.stock === data[0].symbol
+                    ? data[0]
+                    : data[1];
+                const stockNotInDB =
+                  docs[0].stockData.stock === data[0].symbol
+                    ? data[1]
+                    : data[0];
+                const validVoteIp =
+                  !docs[0].stockData.voters.includes(ip) && like ? ip : false;
+
+                // Need to check if stocks are the same and either compare them or say they are the same
                 Promise.all([
                   dbController.updateOne(stockInDB, validVoteIp),
                   dbController.insertOne(stockNotInDB, voteIp)
@@ -107,11 +117,19 @@ module.exports = function(app) {
               }
 
               if (docs.length === 2) {
-                console.log('Both stocks are in the database');
-                const firstVote = !docs[0].stockData.voters.includes(ip) && like ? ip : false;
-                const secondVote = !docs[1].stockData.voters.includes(ip) && like ? ip : false;
-                const firstStock = docs[0].stockData.stock === data[0].symbol ? data[0] : data[1];
-                const secondStock = docs[0].stockData.stock === data[0].symbol ? data[1] : data[0];
+                console.log("Both stocks are in the database");
+                const firstVote =
+                  !docs[0].stockData.voters.includes(ip) && like ? ip : false;
+                const secondVote =
+                  !docs[1].stockData.voters.includes(ip) && like ? ip : false;
+                const firstStock =
+                  docs[0].stockData.stock === data[0].symbol
+                    ? data[0]
+                    : data[1];
+                const secondStock =
+                  docs[0].stockData.stock === data[0].symbol
+                    ? data[1]
+                    : data[0];
 
                 Promise.all([
                   dbController.updateOne(firstStock, firstVote),
@@ -133,15 +151,15 @@ module.exports = function(app) {
         })
         .catch(error => {
           if (error) console.log(error);
-          res.send('Invalid stock');
+          res.send("Invalid stock");
         });
     });
 
     app.use(function(req, res, next) {
       res
         .status(404)
-        .type('text')
-        .send('Not Found');
+        .type("text")
+        .send("Not Found");
     });
   });
 };
